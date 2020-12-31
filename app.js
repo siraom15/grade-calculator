78e419f7bdd342c94992d6167d6a561642f1c78e
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-12-31 07:13:42 +0700

    [ADD] index/app

---
+++ app.js
@@ -0,0 +1,81 @@
+// var subject_count = 1;
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        subject_count: 1,
+        grade: [
+            {
+                // subject_id: 1,
+                subject_name: null,
+                credit: 1,
+                grade: null
+            }
+        ],
+        sumOfGrade: 0,
+        sumOfCredit: 0,
+        avgGrade: 0
+    },
+    methods: {
+        addSubject: function () {
+            this.subject_count++;
+            this.createNewSubject()
+            console.log(this.grade)
+        },
+        removeSubject: function (index) {
+            this.subject_count--
+            this.grade.splice(index, 1)
+        },
+        createNewSubject: function () {
+            let temp = {
+                // subject_id: this.subject_count,
+                subject_name: null,
+                credit: 1,
+                grade: null
+            }
+            this.grade.push(temp)
+        },
+        calculateGrade: function () {
+            let sumOfCredit = 0
+            let sumOfGrade = 0
+            for (let index = 0; index < this.grade.length; index++) {
+                // const element = array[index];
+                if (this.grade[index].grade != null) {
+                    sumOfCredit -= -(this.grade[index].credit)
+                    sumOfGrade -= -(this.grade[index].credit * this.grade[index].grade)
+                }
+            }
+            console.log("sumC" + sumOfCredit);
+            console.log("sumG" + sumOfGrade);
+
+            this.sumOfCredit = sumOfCredit
+            this.sumOfGrade = sumOfGrade
+            this.avgGrade = sumOfGrade / sumOfCredit
+            console.log(this.avgGrade);
+        }
+    },
+    watch: {
+        grade: {
+            handler: function () {
+                let sumOfCredit = 0
+                let sumOfGrade = 0
+                for (let index = 0; index < this.grade.length; index++) {
+                    // const element = array[index];
+                    if (this.grade[index].grade != null) {
+                        sumOfCredit -= -(this.grade[index].credit)
+                        sumOfGrade -= -(this.grade[index].credit * this.grade[index].grade)
+                    }
+                }
+                console.log("sumC" + sumOfCredit);
+                console.log("sumG" + sumOfGrade);
+
+                this.sumOfCredit = sumOfCredit
+                this.sumOfGrade = sumOfGrade
+                this.avgGrade = sumOfGrade / sumOfCredit
+                console.log(this.avgGrade);
+            },
+            deep: true
+        }
+    }
+})
